8648648a013f559d6e4d49b796d7363731f952ca
後藤悠伽 <haruka@gotouyuutoginoMacBook-Pro.local>
2019-04-07 03:37:29 +0900

    pre chart

--- js/chart.js
+++ js/chart.js
@@ -1,17 +1,18 @@
 var text = $(document.getElementById('text'));
-var question = 1;
+q = 1; //questionの略
 
 //yesボタンが押されたら
 $('#yes').click(function(){
   //次のページへ行く処理
-  if(question==1){
-    this.question += 1 ; //2ページへ
+  if(q==1){
+    q += 1 ; //2ページへ
     text.html( "カキクケコ" );
     return;
-  }else if(quesution==2){
-    this.question +=2; //4へ
+  }else if(q==2){
+    q +=2; //4へ
     text.html( "タチツテト" );
-    returm;
+    $('#result').show();
+    return;
   }
   return;
 });
@@ -19,8 +20,12 @@ $('#yes').click(function(){
 //noボタンが押されたら
 $('#no').click(function(){
   //次のページへ行く処理
-  if(question==1){
-    question +1 ; //2ページへ
+  if(q==1){
+    q +1 ; //2ページへ
     return;
   }
+});
+
+$("#result").click(function() {
+  window.location.href = 'https://prog-8.com/slides?lesson=13%2C21%2C20&word=';
 });
